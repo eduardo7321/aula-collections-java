0c09179db5c83b9267c974beb1af39f802493095
eduardo7321 <eduardo.jose184@gmail.com>
2021-07-19 22:43:33 -0300

    aula de list

---
+++ src/test/java/com/estudo/collections/java/estudocollectionsjava/EstudoCollectionsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.collections.java.estudocollectionsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoCollectionsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
